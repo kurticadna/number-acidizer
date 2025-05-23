c58e5792f49f9597b45ae973e5170dae87564ef7
Adna Kurtic <atahic1@gmail.com>
2025-05-23 10:37:55 +0200

    fix:setting up AWS resources

---
+++ infrastructure/setup.sh
@@ -0,0 +1,7 @@
+
+#!/bin/bash
+
+aws ecr create-repository --repository-name acidizer-dev-backend 2>/dev/null
+aws dynamodb create-table --table-name acidizer-dev-counter --attribute-definitions AttributeName=id,AttributeType=S --key-schema AttributeName=id,KeyType=HASH --billing-mode PAY_PER_REQUEST 2>/dev/null
+aws iam create-role --role-name acidizer-dev-lambda-role --assume-role-policy-document '{"Version":"2012-10-17","Statement":[{"Effect":"Allow","Principal":{"Service":"lambda.amazonaws.com"},"Action":"sts:AssumeRole"}]}' 2>/dev/null
+aws iam put-role-policy --role-name acidizer-dev-lambda-role --policy-name acidizer-dev-lambda-policy --policy-document '{"Version":"2012-10-17","Statement":[{"Effect":"Allow","Action":["logs:CreateLogGroup","logs:CreateLogStream","logs:PutLogEvents"],"Resource":"arn:aws:logs:*:*:*"},{"Effect":"Allow","Action":["dynamodb:GetItem","dynamodb:UpdateItem"],"Resource":"arn:aws:dynamodb:us-east-1:*:table/acidizer-dev-counter"}]}'
